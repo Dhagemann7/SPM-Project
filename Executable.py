44b9fd7d45f1cf81828f267f9804dd98508bbc5c
Dave Hagemann <36168264+Dhagemann7@users.noreply.github.com>
2020-11-24 18:50:08 -0500

    Gui first draft
    Here's the first version of the GUI class and implementation in main. Much more work to be done.

--- Executable.py
+++ Executable.py
@@ -1,7 +1,10 @@
 import tkinter
 import gui_util
 from gui_util import AppWindow
-from tkinter import *
+from tkinter import Tk
+#For some reason, JUST importing this module makes the maximize function in Tkinter use the correct screen resolution.
+import pyautogui
+
 #This will act as the main for the program.
 
 #To my understanding, this line as well as mainloop(At the end of main) bookend the program main.
@@ -11,8 +14,8 @@ mainWindow = tkinter.Tk()
 #This initializes the GUI_Utilities class, most functions that interact with tkinter should be clustered in there as functions.
 App = AppWindow(master=mainWindow)
 App.editTitle('Project Tracker')
-#This frame can hold any menus we need
-MenuFrame = App.createFrame(parent=App, side = 'top')
-ScrollFrame = App.createFrame(parent=App, side = 'right')
+
+#Here's an example of how to put in things into the window.
+BackgroundImage = App.enterImage(mainWindow, 'Temp Button-1.png', 'top', App.width, App.height)
 
 App.mainloop()
