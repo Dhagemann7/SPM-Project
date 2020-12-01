ddffe74f80d4e719e5c897036eb5a401cb7181de
Haiden <hgembinski@gmail.com>
2020-11-30 23:29:52 -0500

    commands parameters / remove deprecated code

--- Commands.py
+++ Commands.py
@@ -14,88 +14,47 @@ def removeMenu(buttons):
     buttons['AddHours'].place_forget()
     return
 
-def AddProject():
-    #removeMenu(buttons) - To Remove Main Menu
-
-    #create input fields
+def AddProject(name,owner,members,description):
+    #create Project object from inputs (confirm button) -> go to project view
         #Project Name
         #Project Owner
         #Project Team Members
         #Project Description
 
-    #get input from fields
-
-    #create Project object from inputs (confirm button) -> go to project view
-
-    #if exit/back button pressed, return to main menu
+    #go to project view
 
     return
 
-def AddRisks():
-    #removeMenu(buttons) - To Remove Main Menu
-
-    #creates input fields
+def AddRisks(name,status):
+    #adds risks to project (confirm button)
         #Risk Name
         #Risk Status
 
-    #adds risks to project (confirm button)
-
-    #if exit/back button pressed, return to project view
-
-def AddRequirements():
-    #removeMenu(buttons) - To Remove Main Menu
-
-    #create input fields
-        #Functional Requirements
-        #Non-Functional Requirements
-
-    #get input from fields
-
-    #add requirements to project (confirm button) -> return to project view
-
-    #if exit/back button pressed, return to project view
+    #return to project view
 
     return
 
-def AddHours():
-    #removeMenu(buttons) - To Remove Main Menu
+def AddRequirements(functional,nonfunctional):
+    #add requirements to project (confirm button) -> 
+        #Functional Requirements
+        #Non-Functional Requirements
 
-    #create input fields
-        #Week #
+    # return to project view
+
+    return
+
+def AddHours(week,requirementsHours,designHours,codingHours,testingHours,projManagementHours):
+    #add hours to project (confirm button)
+        #Week Number
         #Requirements Analysis Hours
         #Design Hours
         #Coding Hours
         #Testing Hours
         #Project Management Hours
 
-    #get input from fields
-
-    #add week to project (confirm button) -> return to project view
-
-    #if exit/back button pressed, return to main menu
+    #return to project view
 
     return
-
-def ViewProject():
-    #removeMenu(buttons) - To Remove Main Menu
-
-    #prompts user for project to display
-    #display that project's info + risks + requirements
-
-    #if exit/back button pressed, return to main menu
-
-    return
-
-def ViewHours():
-    #removeMenu(buttons) - To Remove Main Menu
-
-    #display project hours organized by type
-
-    #if exit/back button pressed, return to project view
-
-    return
-
-
 
 #def Writer(i, input):
 #    projectwriter = Project()
